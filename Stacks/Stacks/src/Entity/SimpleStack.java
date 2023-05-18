44c2805e23b0176fbac52045613cc21364c8027e
Arend <matheus.arend@univille.br>
2023-05-18 18:55:25 -0300

    Remove last and add first

--- Stacks/Stacks/src/Entity/SimpleStack.java
+++ Stacks/Stacks/src/Entity/SimpleStack.java
@@ -5,7 +5,7 @@ public class SimpleStack {
     public SimpleStack() {}
 
 
-    public int[] Push(int recivedNumber, int[] recivedStack)
+    public int[] AddLast(int recivedNumber, int[] recivedStack)
     {
 
         //Cria uma nova pilha.
@@ -29,6 +29,53 @@ public class SimpleStack {
         return sentStack;
 
     }
+
+
+    public int[] RemoveLast(int[] recivedStack)
+    {
+
+        int[] sentStack = new int[(recivedStack.length - 1)];
+
+        for(int a = 0; a < recivedStack.length; a++)
+        {
+
+            //Verifica se é a ultima posição da pilha.
+            if(a != (recivedStack.length - 1))
+            {
+                //Coloca os valores da pilha recebida na nova pilha.
+                sentStack[a] = recivedStack[a];
+            }
+
+        }
+
+        return sentStack;
+
+    }
+
+
+    public int[] AddFirst(int recivedNumber, int[] recivedStack)
+    {
+
+        //Cria uma nova pilha.
+        int[] sentStack = new int[recivedStack.length + 1];
+
+        for(int a = 0; a < sentStack.length; a++)
+        {
+
+            if(a == 0)
+            {
+                sentStack[a] = recivedNumber;
+            }else
+            {
+                sentStack[a] = recivedStack[a - 1];
+            }
+
+        }
+
+        return sentStack;
+
+    }
+
 
 
 
--- Stacks/Stacks/src/Main.java
+++ Stacks/Stacks/src/Main.java
@@ -5,19 +5,20 @@ public class Main {
     {
 
 
-        SimpleStack pilhaMinima = new SimpleStack();
+//---------------- Adicionando último ----------------
+
+        SimpleStack pilhaSimples = new SimpleStack();
+
+        int[] stack = new int[2];
+
+        stack[0] = 3;
+        stack[1] = 6;
+
+        stack = pilhaSimples.AddLast(9, stack);
 
 
-        int[] arraySent = new int[2];
-
-        arraySent[0] = 3;
-        arraySent[1] = 6;
-
-        int[] stack = pilhaMinima.Push(9, arraySent);
-
-
-        System.out.println(String.format("Array (%s):", stack.length));
-
+        //Mostrando o array
+        System.out.println(String.format("Add last (%s):", stack.length));
         for(int a = 0; a < stack.length; a++){
 
             if(a == 0){
@@ -32,6 +33,47 @@ public class Main {
 
         }
 
+
+//---------------- Removendo o último ----------------
+
+        stack = pilhaSimples.RemoveLast(stack);
+
+        //Mostrando o array
+        System.out.println(String.format("Remove last (%s):", stack.length));
+        for(int a = 0; a < stack.length; a++){
+
+            if(a == 0){
+                System.out.print("[" + stack[a]);
+            }else{
+                System.out.print(", " + stack[a]);
+            }
+
+            if(a == (stack.length - 1)){
+                System.out.println("]\n");
+            }
+
+        }
+
+
+//---------------- Adicionando o primeiro ----------------
+
+        stack = pilhaSimples.AddFirst(9, stack);
+
+        //Mostrando o array
+        System.out.println(String.format("Add first (%s):", stack.length));
+        for(int a = 0; a < stack.length; a++){
+
+            if(a == 0){
+                System.out.print("[" + stack[a]);
+            }else{
+                System.out.print(", " + stack[a]);
+            }
+
+            if(a == (stack.length - 1)){
+                System.out.println("]\n");
+            }
+
+        }
 
     }
 }
